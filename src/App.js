46002abc9b8121662cd72b8c7b68149525d73254
Fuad Baskara P <fuadbaskara@gmail.com>
2018-07-06 23:59:13 +0700

    HashRouter

--- src/App.js
+++ src/App.js
@@ -3,23 +3,20 @@ import "./styles/App.css";
 import "./styles/Step2.css";
 import "./styles/Responsive.css";
 import ScrollToTop from "./ScrollToTop";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import Main from "./components/Main";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Router>
+        <HashRouter>
           <ScrollToTop>
             <Switch>
               <Route exact path="/" component={Main} />
             </Switch>
           </ScrollToTop>
-        </Router>
-      </div>
+        </HashRouter>
     );
   }
 }
